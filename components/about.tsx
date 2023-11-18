75c950dcf2ffe09e59f9ddac3e19a982dd32943f
SaudSaleem <sauddsaleem@gmail.com>
2023-11-18 15:03:57 +0500

    Update portfolio data

--- components/about.tsx
+++ components/about.tsx
@@ -19,7 +19,7 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
+        {/* After graduating with a degree in{" "}
         <span className="font-medium">Bachelor Of Computer Science</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
@@ -33,17 +33,30 @@ export default function About() {
         . I am also familiar with TypeScript. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
-        developer.
+        developer. */}
+              5+ years experience in Front-End Development and Full Stack Development with a strong background in
+      delivering cutting-edge features. Proficient in Vue, React, Angular, Node, and AWS technologies. Skilled in
+      designing and building frontend core features, optimizing code for non-blocking performance, and modernizing
+      legacy codebases into modern frameworks.
       </p>
-
-      <p>
+      <SectionHeading>Achievements</SectionHeading>
+      <ul style={{listStyle: 'unset'}}>
+        <li style={{textAlign: 'start',fontSize: '14px'}}> Reduce 60% of the loading time of apps by converting legacy code into a modern framework.</li>
+        <li style={{textAlign: 'start',fontSize: '14px'}}>Attract 100% more agents from the past by giving one of my project (Real Estate app) a new UI look and adding new features.</li>
+        <li style={{textAlign: 'start',fontSize: '14px'}}>Help one of my client organization to become the #2 real estate app in New York by continuous efforts in the tech area.</li>
+        <li style={{textAlign: 'start',fontSize: '14px'}}>10 million New York residents using my newly developed feature comparative market analysis (CMA), which
+      helps them to find rental and sale buildings by comparing pricing for similar buildings in neighborhoods.</li>
+        <li style={{textAlign: 'start',fontSize: '14px'}}> Automate 90% of one of my organization's day-to-day office tasks by developing an in-house SaaS system.</li>
+        <li style={{textAlign: 'start',fontSize: '14px'}}>Cut the one of my previous organization's 100% monthly expenses allocated for Slack. We developed an inhouse chat application for inter-communications between teams and employees.</li>
+      </ul>
+      {/* <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>. I'm also
         learning how to play the guitar.
-      </p>
+      </p> */}
     </motion.section>
   );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,7 @@ import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import hlresImg from "@/public/hlres.png";
 
 export const links = [
   {
@@ -35,20 +36,12 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Lahore, PK",
+    title: "Full-Stack Developer",
+    location: "USA, Remote",
     description:
-      "I have a bachelor's degree in computer science. Major subjects of degree are OOP, Data Structure And Algorithms, Web Development, Compiler Construction, Operation System, Computer Networking, Data Science, Machine Learning, and Theory of Automata.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2020",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Lahore, PK",
-    description:
-      "I worked as a Software Engineer in Programmers Force. It has headquartered in the United Kingdom. It specialise in Application Programming Interface (API) Development and Integration, Fintech Solutions, Enterprise Applications, Mobile App Development, and Big Data Processing to facilitate several corporate clients.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2020 - 2022",
+      "Working as a Senior Frontend Engineer. Helping organisation to deliver state of the art feature for real estate domain. Technologies Vue, Django, AWS.",
+    icon: React.createElement(FaReact),
+    date: "2022 - 2023",
   },
   {
     title: "Full-Stack Developer",
@@ -59,22 +52,34 @@ export const experiencesData = [
     date: "2022 - 2023",
   },
   {
-    title: "Full-Stack Developer",
-    location: "USA, Remote",
+    title: "Front-End Developer",
+    location: "Lahore, PK",
     description:
-      "Working as a Senior Frontend Engineer. Helping organisation to deliver state of the art feature for real estate domain. Technologies Vue, Django, AWS.",
-    icon: React.createElement(FaReact),
-    date: "2022 - 2023",
+      "I worked as a Software Engineer in Programmers Force. It has headquartered in the United Kingdom. It specialise in Application Programming Interface (API) Development and Integration, Fintech Solutions, Enterprise Applications, Mobile App Development, and Big Data Processing to facilitate several corporate clients.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2020 - 2022",
   },
+  {
+    title: "Graduated bootcamp",
+    location: "Lahore, PK",
+    description:
+      "I have a bachelor's degree in computer science. Major subjects of degree are OOP, Data Structure And Algorithms, Web Development, Compiler Construction, Operation System, Computer Networking, Data Science, Machine Learning, and Theory of Automata.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2020",
+  },
+
+  
+
 ] as const;
 
 export const projectsData = [
   {
-    title: "WhistleIt",
+    title: "Highline Residential",
     description:
-      "I worked as a Frontend developer on this startup project for 2 years. Users can communicate just like slack.",
-    tags: ["Vue", "Veutify", "MongoDB", "Bootsrap", "Node", "Docker"],
-    imageUrl: corpcommentImg,
+      "Working as a Senior Frontend Engineer. Helping organizations to deliver state-of-the-art features for the real estate domain. Technologies Vue, Django, AWS, Webpack.",
+    tags: ["Vue", "Django", "Ag-Grid", "Webpack"],
+    imageUrl: hlresImg,
+    projectUrl: 'https://www.hlres.com/'
   },
   {
     title: "Invohub",
@@ -82,6 +87,15 @@ export const projectsData = [
       "Invohub is developed for managing resources data. Such as managing resources salaries, theirs increment record, performance in different projects, attendance, remaining annual, casual, sick leaves.",
     tags: ["React", "Node", "Postgress", "Next", "Redux"],
     imageUrl: rmtdevImg,
+    projectUrl: 'https://hub.invozone.com/'
+  },
+  {
+    title: "WhistleIt",
+    description:
+      "I worked as a Frontend developer on this startup project for 2 years. Users can communicate just like slack.",
+    tags: ["Vue", "Veutify", "MongoDB", "Bootsrap", "Node", "Docker"],
+    imageUrl: corpcommentImg,
+    projectUrl: 'https://app.whistleit.io/'
   },
   {
     title: "Gigimot",
@@ -89,13 +103,7 @@ export const projectsData = [
       "Gigimot is online video meeting app, which gives users flexibility to arrange one to one or group recurring virtual meetings. This product is used by health and wellness coaches, teachers.",
     tags: ["Vue", "Node", "Postgres",],
     imageUrl: wordanalyticsImg,
-  },
-  {
-    title: "Unchained Carrot",
-    description:
-      "Unchained Carrot is unique project that gives companies and marketers the ability to easily create reward-based marketing campaigns.",
-    tags: ["Vue", "Veutify",],
-    imageUrl: wordanalyticsImg,
+    projectUrl: 'https://gigimot.com/'
   },
 ] as const;
 
